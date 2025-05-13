13262384b3fbb94fb6c9e03d7dc9883334b62e58
Khanel <m.khanloo.shendy@gmail.com>
2025-05-13 16:36:32 +0330

    feat(core): initialize Hive in main entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
-import 'package:flutter/material.dart';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  // TODO: Register adapters and open boxes as needed for DI
   runApp(const MyApp());
 }
 
